c27c845e886d6314c92ce08b6fcdd910d5200f6c
David Kreismann <wi22230@lehre.dhbw-stuttgart.de>
2024-12-03 15:22:46 +0100

    cf

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+
+/** @type {import("next").NextConfig} */
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+    unoptimized: true,
+  },
+  // Already doing linting and typechecking as separate tasks in CI
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+}
+
+export default config
