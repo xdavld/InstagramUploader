93630b3949db523305ccfbeea56dfd8a2364e4f5
Leonard Eckert <wi22219@lehre.dhbw-stuttgart.de>
2025-01-10 13:31:14 +0100

    Stop tracking .next directory

--- .next/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_document.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules__pnpm_4ed908._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__f41634._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.0.3_react-dom@19.0.0-rc-fb9a90fa48-20240614_react@19.0.0-rc-fb9a90fa48-20240614__reac_3oppydrhzshrauvez7b6rgn7pi/node_modules/next/document.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9ZiJsIQV90vbucm/ojvEf8Xh+PxK7ShTevonWh3Ssfc=\"\n}"
