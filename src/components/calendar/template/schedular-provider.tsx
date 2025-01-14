2d958bfc1d495f2b51807af053d3707e059f2fc1
Nico Wagner <wi22010@lehre.dhbw-stuttgart.de>
2025-01-14 23:09:46 +0100

    Calendar

---
+++ src/components/calendar/template/schedular-provider.tsx
@@ -0,0 +1,295 @@
+"use client";
+
+// SchedulerContext.tsx
+import React, {
+  createContext,
+  useContext,
+  useReducer,
+  ReactNode,
+  useEffect,
+} from "react";
+import { ModalProvider } from "./modal-provider";
+import {
+  Action,
+  Event,
+  Getters,
+  Handlers,
+  SchedulerContextType,
+  startOfWeek,
+} from "@/components/calendar/template/types";
+// Define event and state types
+
+interface SchedulerState {
+  events: Event[];
+}
+
+// Define the variant options
+export const variants = [
+  "success",
+  "primary",
+  "default",
+  "warning",
+  "danger",
+] as const;
+
+// Initial state
+const initialState: SchedulerState = {
+  events: [],
+};
+
+// Reducer function
+const schedulerReducer = (
+  state: SchedulerState,
+  action: Action
+): SchedulerState => {
+  switch (action.type) {
+    case "ADD_EVENT":
+      return { ...state, events: [...state.events, action.payload] };
+
+    case "REMOVE_EVENT":
+      return {
+        ...state,
+        events: state.events.filter((event) => event.id !== action.payload.id),
+      };
+    case "UPDATE_EVENT":
+      return {
+        ...state,
+        events: state.events.map((event) =>
+          event.id === action.payload.id ? action.payload : event
+        ),
+      };
+    case "SET_EVENTS":
+      return { ...state, events: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+// Create the context with the correct type
+const SchedulerContext = createContext<SchedulerContextType | undefined>(
+  undefined
+);
+
+// Provider component
+export const SchedulerProvider = ({
+  children,
+  onAddEvent,
+  onUpdateEvent,
+  onDeleteEvent,
+  initialState,
+  weekStartsOn = "monday",
+}: {
+  onAddEvent?: (event: Event) => void;
+  onUpdateEvent?: (event: Event) => void;
+  onDeleteEvent?: (id: string) => void;
+  weekStartsOn?: startOfWeek;
+  children: ReactNode;
+  initialState?: Event[];
+}) => {
+  const [state, dispatch] = useReducer(
+    schedulerReducer,
+    { events: initialState ?? [] } // Sets initialState or an empty array as the default
+  );
+
+  useEffect(() => {
+    if (initialState) {
+      dispatch({ type: "SET_EVENTS", payload: initialState });
+    }
+  }, [initialState]);
+
+  // global getters
+  const getDaysInMonth = (month: number, year: number) => {
+    return Array.from(
+      { length: new Date(year, month + 1, 0).getDate() },
+      (_, index) => ({
+        day: index + 1,
+        events: [],
+      })
+    );
+  };
+
+  const getDaysInWeek = (week: number, year: number) => {
+    // Determine if the week should start on Sunday (0) or Monday (1)
+    const startDay = weekStartsOn === "sunday" ? 0 : 1;
+
+    // Get January 1st of the year
+    const janFirst = new Date(year, 0, 1);
+
+    // Calculate how many days we are offsetting from January 1st
+    const janFirstDayOfWeek = janFirst.getDay();
+
+    // Calculate the start of the week by finding the correct day in the year
+    const weekStart = new Date(janFirst);
+    weekStart.setDate(
+      janFirst.getDate() +
+        (week - 1) * 7 +
+        ((startDay - janFirstDayOfWeek + 7) % 7)
+    );
+
+    // Generate the week’s days
+    const days = [];
+    for (let i = 0; i < 7; i++) {
+      const day = new Date(weekStart);
+      day.setDate(day.getDate() + i);
+      days.push(day);
+    }
+
+    return days;
+  };
+
+  const getWeekNumber = (date: Date) => {
+    const d = new Date(
+      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+    );
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    const weekNo = Math.ceil(
+      ((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7
+    );
+    return weekNo;
+  };
+
+  // Helper function to filter events for a specific day
+  const getEventsForDay = (day: number, currentDate: Date) => {
+    return state?.events.filter((event) => {
+      const eventStart = new Date(event.startDate);
+      const eventEnd = new Date(event.endDate);
+
+      // Create new Date objects to avoid mutating `currentDate`
+      const startOfDay = new Date(currentDate);
+      startOfDay.setDate(day);
+      startOfDay.setHours(0, 0, 0, 0);
+
+      const endOfDay = new Date(currentDate);
+      endOfDay.setDate(day + 1);
+      endOfDay.setHours(0, 0, 0, 0);
+
+      // Check if the event starts or spans across the given day
+      const isSameDay =
+        eventStart.getDate() === day &&
+        eventStart.getMonth() === currentDate.getMonth() &&
+        eventStart.getFullYear() === currentDate.getFullYear();
+
+      const isSpanningDay = eventStart < endOfDay && eventEnd >= startOfDay;
+
+      return isSameDay || isSpanningDay;
+    });
+  };
+
+  const getDayName = (day: number) => {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  };
+
+  const getters: Getters = {
+    getDaysInMonth,
+    getEventsForDay,
+    getDaysInWeek,
+    getWeekNumber,
+    getDayName,
+  };
+
+  // handlers
+  function handleEventStyling(event: Event, dayEvents: Event[]) {
+    const eventsOnHour = dayEvents.filter((e) => {
+      return (
+        e.startDate < event.endDate && e.endDate > event.startDate // Any overlap
+      );
+    });
+
+    const numEventsOnHour = eventsOnHour.length || 1;
+    const indexOnHour = eventsOnHour.indexOf(event);
+
+    let eventHeight = 0;
+    let maxHeight = 0;
+    let eventTop = 0;
+
+    if (event.startDate instanceof Date && event.endDate instanceof Date) {
+      // Normalize start and end dates to only include hours and minutes
+      const startTime =
+        event.startDate.getHours() * 60 + event.startDate.getMinutes(); // Convert to minutes
+      const endTime =
+        event.endDate.getHours() * 60 + event.endDate.getMinutes(); // Convert to minutes
+
+      // Calculate the difference in minutes between start and end times
+      const diffInMinutes = endTime - startTime;
+
+      // Calculate the event height based on the duration (64px per hour, so 64px/60min = 1.0667px per minute)
+      eventHeight = (diffInMinutes / 60) * 64;
+      // console.log("eventHeight", eventHeight);
+
+      // Get the event start hour as a fraction (e.g., 13.5 for 13:30)
+      const eventStartHour =
+        event.startDate.getHours() + event.startDate.getMinutes() / 60;
+
+      // Define the day-end hour (24.0 for midnight)
+      const dayEndHour = 24;
+
+      // Calculate maxHeight based on the difference between the day-end hour and the event's start hour
+      maxHeight = Math.max(0, (dayEndHour - eventStartHour) * 64);
+
+      // Limit the event height to the calculated maxHeight (so it doesn’t overflow beyond the day)
+      eventHeight = Math.min(eventHeight, maxHeight);
+
+      // Calculate the top position based on the event's start time (64px per hour)
+      eventTop = eventStartHour * 64;
+    } else {
+      console.error("Invalid event or missing start/end dates.");
+    }
+
+    return {
+      height: `${eventHeight < 10 ? 20 : eventHeight > maxHeight ? maxHeight : eventHeight}px`,
+      top: `${eventTop}px`,
+      zIndex: indexOnHour + 1,
+      left: `${(indexOnHour * 100) / numEventsOnHour}%`,
+      maxWidth: `${100 / numEventsOnHour}%`,
+      minWidth: `${100 / numEventsOnHour}%`,
+    };
+  }
+
+  function handleAddEvent(event: Event) {
+    dispatch({ type: "ADD_EVENT", payload: event });
+    if (onAddEvent) {
+      onAddEvent(event);
+    }
+  }
+
+  function handleUpdateEvent(event: Event, id: string) {
+    dispatch({ type: "UPDATE_EVENT", payload: { ...event, id } });
+    if (onUpdateEvent) {
+      onUpdateEvent(event);
+    }
+  }
+
+  function handleDeleteEvent(id: string) {
+    dispatch({ type: "REMOVE_EVENT", payload: { id } });
+    if (onDeleteEvent) {
+      onDeleteEvent(id);
+    }
+  }
+
+  const handlers: Handlers = {
+    handleEventStyling,
+    handleAddEvent,
+    handleUpdateEvent,
+    handleDeleteEvent,
+  };
+
+  return (
+    <SchedulerContext.Provider
+      value={{ events: state, dispatch, getters, handlers, weekStartsOn }}
+    >
+      <ModalProvider>{children}</ModalProvider>
+    </SchedulerContext.Provider>
+  );
+};
+
+// Custom hook to use the scheduler context
+export const useScheduler = () => {
+  const context = useContext(SchedulerContext);
+  if (!context) {
+    throw new Error("useScheduler must be used within a SchedulerProvider");
+  }
+  return context;
+};
---
+++ src/components/calendar/template/select-date.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { EventFormData } from "@/components/calendar/template/types";
+import { parseDate, CalendarDate, Time } from "@internationalized/date";
+import { DateRangePicker } from "@nextui-org/react";
+import { TimeInput } from "@nextui-org/react";
+
+import React, { useEffect, useState } from "react";
+import { UseFormSetValue } from "react-hook-form";
+
+function getFormattedDate(date: Date) {
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(
+    date.getDate()
+  ).padStart(2, "0")}`;
+}
+
+// Convert CalendarDate to JavaScript Date
+function calendarDateToJSDate(calendarDate: CalendarDate): Date {
+  const { year, month, day } = calendarDate;
+  return new Date(year, month - 1, day); // JS Date uses 0-indexed months
+}
+
+export default function SelectDate({
+  data,
+  setValue,
+}: {
+  data?: { startDate: Date; endDate: Date; time: Time };
+  setValue: UseFormSetValue<EventFormData>;
+}) {
+  const [dateState, setDateState] = useState({
+    startDate: data
+      ? parseDate(getFormattedDate(data?.startDate))
+      : parseDate("2024-04-06"),
+    endDate: data
+      ? parseDate(getFormattedDate(data?.endDate))
+      : parseDate("2024-04-10"),
+
+    startTime: new Time(
+      data?.startDate?.getHours() || 0,
+      data?.startDate?.getMinutes() || 0
+    ),
+    endTime: new Time(
+      data?.endDate?.getHours() || 0,
+      data?.endDate?.getMinutes() || 0
+    ),
+  });
+
+  useEffect(() => {
+    if (!dateState) return;
+
+    const jsStartDate = calendarDateToJSDate(dateState.startDate);
+    const jsEndDate = calendarDateToJSDate(dateState.endDate);
+
+    // add the time to the date
+    jsStartDate.setHours(dateState?.startTime?.hour || 0);
+    jsStartDate.setMinutes(dateState?.startTime?.minute || 0);
+
+    jsEndDate.setHours(dateState?.endTime?.hour || 0);
+    jsEndDate.setMinutes(dateState?.endTime?.minute || 0);
+
+    // check if the end date is before the start date
+    if (jsEndDate < jsStartDate) {
+      jsEndDate.setHours(jsStartDate.getHours() + 1);
+    }
+
+    setValue("startDate", jsStartDate);
+    setValue("endDate", jsEndDate);
+  }, [dateState, setValue]);
+
+  return (
+    <div>
+      <div className="w-full flex gap-4 max-w-full flex-wrap">
+        <DateRangePicker
+          label="Stay duration"
+          isRequired
+          value={{ start: dateState.startDate, end: dateState.endDate }}
+          className="w-full"
+          onChange={(value) => {
+            const start = value?.start;
+            const end = value?.end;
+
+            const startDate = new Date(
+              start?.year || 0,
+              (start?.month || 1) - 1,
+              start?.day || 1
+            );
+
+            const endDate = new Date(
+              end?.year || 0,
+              (end?.month || 1) - 1,
+              end?.day || 1
+            );
+
+            setDateState({
+              ...dateState,
+              startDate: parseDate(getFormattedDate(startDate)),
+              endDate: parseDate(getFormattedDate(endDate)),
+            });
+          }}
+        />
+
+        <div className="flex flex-wrap gap-4">
+          <TimeInput
+            label="Start Time"
+            defaultValue={dateState?.startTime}
+            onChange={(e: Time) => {
+              setDateState({
+                ...dateState,
+                startTime: e,
+              });
+            }}
+          />
+
+          <TimeInput
+            label="End Time"
+            defaultValue={dateState?.endTime}
+            onChange={(e: Time) => {
+              setDateState({
+                ...dateState,
+                endTime: e,
+              });
+            }}
+            isInvalid={
+              dateState?.startTime &&
+              dateState?.endTime &&
+              dateState.endTime.hour * 60 + dateState.endTime.minute <=
+                dateState.startTime.hour * 60 + dateState.startTime.minute
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
