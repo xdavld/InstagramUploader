107ce74707be1034e0a438a69d38bedbc7d73ce4
Nico Wagner <wi22010@lehre.dhbw-stuttgart.de>
2025-01-16 09:58:38 +0100

    Fix

--- src/app/api/instagram/database/insert/route.js
+++ src/app/api/instagram/database/insert/route.js
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 // Supabase-Client initialisieren
 const supabase = createClient(supabaseUrl, supabaseKey);
